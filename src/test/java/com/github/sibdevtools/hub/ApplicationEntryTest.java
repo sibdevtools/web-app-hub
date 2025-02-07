eefc6e74d13881a284c7fed4f6be3e5842bf4909
sibmaks <sibmaks@vk.com>
2025-02-07 13:54:07 +0300

    Add simple integration test

---
+++ src/test/java/com/github/sibdevtools/hub/ApplicationEntryTest.java
@@ -0,0 +1,18 @@
+package com.github.sibdevtools.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author sibmaks
+ * @since 0.0.18
+ */
+@SpringBootTest
+class ApplicationEntryTest {
+    
+    @Test
+    void contextLoads() {
+        // check if the application context loads correctly
+    }
+
+}
